fca04ab189cb1bbbf47d9d23d7566bf61d484e20
Valthor Halldorsson <vlthrh@gmail.com>
2022-10-31 13:52:06 +0100

    allow globals in hotpot macro context

--- init.lua
+++ init.lua
@@ -3,8 +3,27 @@ if pcall(require, "hotpot") then
     require("hotpot").setup({
         enable_hotpot_diagnostics = true,
         provide_require_fennel = true,
+        compiler = {
+            -- options passed to fennel.compile for modules, defaults to {}
+            modules = {
+                -- not default but recommended, align lua lines with fnl source
+                -- for more debuggable errors, but less readable lua.
+                correlate = true
+            },
+            -- options passed to fennel.compile for macros, defaults as shown
+            macros = {
+                env = "_COMPILER", -- MUST be set along with any other options
+                -- you may wish to disable fennels macro-compiler sandbox in some cases,
+                -- this allows access to tables like `vim` or `os` inside macro functions.
+                -- See fennels own documentation for details on these options.
+                compilerEnv = _G,
+                allowGlobals = true,
+            }
+        }
+
     })
     -- Import neovim configuration
+    --
     require("core")
 else
     print("Unable to require hotpot")
